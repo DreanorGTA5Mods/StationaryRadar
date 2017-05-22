0d45eea4223a7563770559c0ec0989263de7a033
Mike <mikeundead@googlemail.com>
2017-05-22 22:20:39 +0200

    cleanup

--- StationaryRadar/server.lua
+++ StationaryRadar/server.lua
@@ -1,17 +1,13 @@
 AddEventHandler('chatMessage', function(source, n, message)
-  local args = stringsplit(message, " ")
+  -- todo only for cops
   if(message == "/set speedradar") then
 	setupCamera(source)
   end
 end)
 
-
 function setupCamera (source)
-
-local b = GET_ENTITY_COORDS(PlayerId(), true)
-	local e = 'chatMessage'
-	local mes = "SYSTEM"
-	local m = "Camera has been set up at"..b
-	local ms = {200, 0, 0}
-    TriggerClientEvent(e, source, mes, ms, m)
+	local b = GET_ENTITY_COORDS(PlayerId(), true)
+	local message = "Camera has been set up at"..b
+	local color = {200, 0, 0}
+    TriggerClientEvent('chatMessage', source, '', color, message)
 end
