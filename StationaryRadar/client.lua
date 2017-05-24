b312db011415e809b179f30e048efd2ce5cf644d
Mike <mikeundead@googlemail.com>
2017-05-24 22:39:16 +0200

    some cleanup

--- StationaryRadar/client.lua
+++ StationaryRadar/client.lua
@@ -1,34 +1,58 @@
 RegisterNetEvent('setRadar')
+RegisterNetEvent('loadRadars')
 
 local speedcams = {};
 local maxSpeedKmh = 1;
 local maxSpeedMph = 10;
 
-AddEventHandler('setRadar', function()
-    --table.insert(speedcams, { key = GetEntityCoords(GetPlayerPed(-1)), value = {0, 1}})
-    speedcams[GetEntityCoords(GetPlayerPed(-1))] = 0
-    --TriggerEvent("chatMessage", "[System]", { 255,0,0}, "Radar set at".." x: ") 
+-----------------------------------------------------------------------
+---------------------Events--------------------------------------------
+-----------------------------------------------------------------------
+
+-- Loads Radars into the client
+AddEventHandler('loadRadars', function(array)
+    speedcams[array] = 0 -- player has not been busted by cam
 end)
 
+-- Manually set Radar
+AddEventHandler('setRadar', function(array)
+    speedcams[GetEntityCoords(GetPlayerPed(-1))] = 0;
+    -- todo Trigger server to save this position
+end)
+
+-- Reguest to get all Radars
+AddEventHandler("playerSpawned", function()
+    TriggerServerEvent("getRadars");
+end)
+
+-----------------------------------------------------------------------
+---------------------Functions-----------------------------------------
+-----------------------------------------------------------------------
+
+-- Determines if player broke speed and message should be triggered
 function SpeedBreak(speedcam, hasBeenFucked, speed, numberplate)
     if speed >= maxSpeedKmh then
         if hasBeenFucked == 0 then
-            speedcams[speedcam] = 1
-            local streethash = GetStreetNameAtCoord(speedcam['x'], speedcam['y'], speedcam['z'])
-            local streetname = GetStreetNameFromHashKey(streethash)
-            local info = string.format("%s | %s mph / %s km/h", numberplate, math.ceil(speed*2.236936), math.ceil(speed*3.6))
-            TriggerEvent("chatMessage", "[System]", { 255,0,0}, string.format("%s | %s | %s mph / %s km/h", streetname, numberplate, math.ceil(speed*2.236936), math.ceil(speed*3.6)))
+            speedcams[speedcam] = 1 -- player got busted by cam
+            local streethash = GetStreetNameAtCoord(speedcam['x'], speedcam['y'], speedcam['z']);
+            local streetname = GetStreetNameFromHashKey(streethash);
+            local info = string.format("%s | %s mph / %s km/h", numberplate, math.ceil(speed*2.236936), math.ceil(speed*3.6));
+
+            -- todo add vehicle name
+            TriggerEvent("chatMessage", "[System]", { 255,0,0}, string.format("%s | %s | %s mph / %s km/h", streetname, numberplate, math.ceil(speed*2.236936), math.ceil(speed*3.6)));
         end
     end
 end
 
+-- Determines if player is close enough to trigger cam
 function HandleSpeedcam(speedcam, hasBeenFucked)
-    local playerPos = GetEntityCoords(GetPlayerPed(-1))
+    local playerPos = GetEntityCoords(GetPlayerPed(-1));
+
     -- Sets radius in which speeding gets triggered
-    local x1 = playerPos['x'] - speedcam['x']
-    local x2 = speedcam['x'] - playerPos['x'] 
-    local y1 = playerPos['y'] - speedcam['y']
-    local y2 = speedcam['y'] - playerPos['y'] 
+    local x1 = playerPos['x'] - speedcam['x'];
+    local x2 = speedcam['x'] - playerPos['x'];
+    local y1 = playerPos['y'] - speedcam['y'];
+    local y2 = speedcam['y'] - playerPos['y'];
     local range = 20;
 
     if y1 <= range 
@@ -38,20 +62,25 @@ function HandleSpeedcam(speedcam, hasBeenFucked)
     then 
         local vehicle = GetPlayersLastVehicle() -- gets the current vehicle the player is in.
         if vehicle ~=nil then
-            local numberplate = GetVehicleNumberPlateText(vehicle)
-			SpeedBreak(speedcam, hasBeenFucked, GetEntitySpeed(vehicle), numberplate)
+            local numberplate = GetVehicleNumberPlateText(vehicle);
+			SpeedBreak(speedcam, hasBeenFucked, GetEntitySpeed(vehicle), numberplate);
         end
     else
-        speedcams[speedcam] = 0 --might remove that for logging of who got fucked where
+        speedcams[speedcam] = 0; --might remove that for logging of who got fucked where
     end
 end
 
+-----------------------------------------------------------------------
+---------------------Threads-------------------------------------------
+-----------------------------------------------------------------------
+
+-- Thread to loop speedcams
 Citizen.CreateThread(function()
     while true do
-        Wait(0)		
+        Wait(0);
         for key, value in pairs(speedcams) do
-                --TriggerEvent("chatMessage", "[System]", { 255,0,0}, "Radar set at".." x: "..key..value) 
-            HandleSpeedcam(key, value)
+            --TriggerEvent("chatMessage", "[System]", { 255,0,0}, "Radar set at".." x: "..key..value)
+            HandleSpeedcam(key, value);
         end
     end  
 end)
--- StationaryRadar/server.lua
+++ StationaryRadar/server.lua
@@ -1,13 +1,19 @@
+-----------------------------------------------------------------------
+---------------------Events--------------------------------------------
+-----------------------------------------------------------------------
+
+-- Chatcommands which can be set by the police
 AddEventHandler('chatMessage', function(source, n, message)
     -- todo only for cops
     if(message == "/set speedradar") then
-        setupCamera(source)
+        TriggerClientEvent('setRadar', source);
     end
 end)
 
-function setupCamera (source)
-    local message = "Camera has been set up at"
-    local color = {200, 0, 0}
-    TriggerClientEvent('chatMessage', source, '', color, message)
-    TriggerClientEvent('setRadar', source)
-end
+-- Loads Radars from Datasource and returns to the player
+RegisterServerEvent('getRadars')
+AddEventHandler('getRadars', function()
+    -- todo load shit from db/file whatever also cache them?
+    local speedcams = { x = 406, y = -968, z = 29 };
+    TriggerClientEvent('loadRadars', source, speedcams);
+end)
